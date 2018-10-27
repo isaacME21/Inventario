be74f5574c1293f8c0dcfd49f04f0b7dfb332361
Isaac <isaac.maya.estrada@gmail.com>
2018-10-27 17:08:17 -0500

    Initial Commit

---
+++ Inventario/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inventario
+//
+//  Created by Luis Isaac Maya on 10/27/18.
+//  Copyright © 2018 Luis Isaac Maya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
